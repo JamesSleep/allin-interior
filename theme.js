b5aa95ef5b099636c22546955d94ad69a223b23d
jamessleep <tyuyiy@naver.com>
2020-07-10 17:00:53 +0900

    App Theme Color Set

---
+++ theme.js
@@ -0,0 +1,8 @@
+import { Dimensions } from "react-native";
+
+export const _WIDTH = Dimensions.get("window").width;
+export const _HEIGHT = Dimensions.get("window").height;
+export const buttonColor = "#EA5420";
+export const nonActive = "#EEEEEE";
+export const backgroundColor = "#ffffff";
+
