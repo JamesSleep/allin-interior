c14c9a19dd2075cb6382148353b794df20563c66
김동하 <dongha@gimdonghaui-Macmini.local>
2020-08-17 21:12:14 +0900

    Safe View Global Patch

--- App.js
+++ App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { NavigationContainer } from "@react-navigation/native";
 import { SafeAreaProvider } from "react-native-safe-area-context";
 import HomeRouter from './src/Router/HomeRouter';
-import { StatusBar } from 'react-native';
+import { StatusBar, SafeAreaView } from 'react-native';
 
 const App = () => {
   return (
     <SafeAreaProvider>
       <NavigationContainer>
         <StatusBar barStyle="dark-content" />
-        <HomeRouter/>
+        <SafeAreaView style={{ flex: 1 }}>
+          <HomeRouter/>
+        </SafeAreaView>
       </NavigationContainer>
     </SafeAreaProvider>
   );
